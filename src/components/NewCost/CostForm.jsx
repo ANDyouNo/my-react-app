994e32f7e929595ed28df4898b2be630fb781ff7
ANDyouNo <younofl312w@gmail.com>
2023-08-29 16:44:51 +0300

    lec 56

--- src/components/NewCost/CostForm.jsx
+++ src/components/NewCost/CostForm.jsx
@@ -73,6 +73,7 @@ function CostForm(props) {
         </div>
         <div className="new-cost__actions">
           <button type="submit">Add Rashod</button>
+          <button type="button" onClick={props.onCancel}>Cancel</button>
         </div>
       </div>
     </form>
